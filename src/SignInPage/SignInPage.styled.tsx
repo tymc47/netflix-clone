85ef3e697907c70503692ab2ae502a5b13b00249
Terry Mak <tymc447@gmail.com>
2022-12-07 22:19:15 +0800

    finish sign in page styles

---
+++ src/SignInPage/SignInPage.styled.tsx
@@ -0,0 +1,112 @@
+import styled from "styled-components";
+
+export const SignInContainer = styled.div`
+  position: relative;
+  background-color: rgba(0, 0, 0, 0.7);
+  border-radius: 4px;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  padding: 60px 68px 40px;
+  font-family: "Netflix Sans Light";
+  color: white;
+  max-width: 450px;
+  min-height: 660px;
+  margin: 0 auto 270px;
+  z-index: 10;
+`;
+
+export const SignInForm = styled.div`
+  flex: 1;
+
+  input {
+    line-height: 25px;
+  }
+
+  button {
+    border-radius: 4px;
+    font-size: 16px;
+    margin: 24px 0 12px;
+    padding: 16px;
+    height: auto;
+  }
+
+  div.login-help {
+    display: flex;
+    font-size: 13px;
+    color: #b3b3b3;
+  }
+`;
+
+export const RememberMe = styled.div`
+  position: relative;
+  flex: 1 0 auto;
+  padding-left: 20px;
+
+  input {
+    position: absolute;
+    opacity: 0;
+    left: 1px;
+  }
+
+  input[type="checkbox"]:checked + label:after {
+    content: "\u2713";
+    font-weight: 900;
+    color: #000;
+    position: absolute;
+    left: 3px;
+  }
+
+  label:before {
+    content: "";
+    position: absolute;
+    background: #737373;
+    border: 0;
+    border-radius: 2px;
+    width: 16px;
+    height: 16px;
+    left: 1px;
+    top: -1px;
+  }
+`;
+
+export const FormOther = styled.div`
+  color: #b3b3b3;
+  font-size: 16px;
+
+  a {
+    text-decoration: none;
+    color: white;
+  }
+
+  div.disclaimer {
+    font-size: 13px;
+
+    button {
+      background-color: transparent;
+      border: none;
+      color: #0071eb;
+      cursor: pointer;
+      padding: 0;
+
+      &.hide {
+        visibility: hidden;
+      }
+    }
+
+    button:hover {
+      text-decoration: underline;
+    }
+  }
+
+  div.hide {
+    opacity: 0;
+  }
+
+  div.disclaimer-text {
+    opacity: 1;
+    a {
+      color: #0071eb;
+    }
+  }
+`;
---
+++ src/SignInPage/index.tsx
@@ -0,0 +1,95 @@
+import NavBar from "../components/NavBar";
+import { RedButton } from "../LandingPage/FrontPage.styled";
+import { useState } from "react";
+import PosterBackground from "../components/PosterBackground";
+import {
+  SignInContainer,
+  SignInForm,
+  RememberMe,
+  FormOther,
+} from "./SignInPage.styled";
+import TextField from "../components/TextField";
+import { validateEmail } from "../utils";
+
+const SignInPage = () => {
+  const [showDisclaimer, setShowDisclaimer] = useState<boolean>(false);
+
+  const usernameValidator = (input: string) => {
+    if (!input) throw new Error("Please enter a valid email or phone number");
+    else if (!validateEmail(input))
+      throw new Error("Please enter a valid email");
+  };
+
+  const passwordValidator = (input: string) => {
+    if (input.length < 4 || input.length > 60)
+      throw new Error(
+        "Your password must contain between 4 and 60 characters."
+      );
+  };
+
+  return (
+    <div>
+      <PosterBackground mode={"fullscreen"} />
+      <NavBar />
+      <SignInContainer>
+        <SignInForm>
+          <h1>Sign In</h1>
+          <form>
+            <TextField
+              label={"Email or phone number"}
+              validator={usernameValidator}
+            />
+            <TextField label={"Password"} validator={passwordValidator} />
+
+            <RedButton>Sign In</RedButton>
+            <div className="login-help">
+              <RememberMe>
+                <input type="checkbox" id="remember-me"></input>
+                <label htmlFor="remember-me">
+                  <span className="remember-me-text">Remember me</span>
+                </label>
+              </RememberMe>
+
+              <a>Need help?</a>
+            </div>
+          </form>
+        </SignInForm>
+        <FormOther>
+          <div>
+            {"New to Netflix? "}
+            <a href="/">Sign up now.</a>
+          </div>
+          <div className="disclaimer">
+            <p>
+              <span>
+                This page is protected by Google reCAPTCHA to ensure you&apos;re
+                not a bot.&nbsp;
+                <button
+                  className={showDisclaimer ? "hide" : ""}
+                  onClick={() => setShowDisclaimer(true)}
+                >
+                  Learn more.
+                </button>
+              </span>
+            </p>
+            <div className={showDisclaimer ? "disclaimer-text" : "hide"}>
+              <span>
+                {
+                  "The information collected by Google reCAPTCHA is subject to the Google "
+                }
+                <a>Privacy Policy</a>
+                {" and "}
+                <a>Terms of Service</a>
+                {
+                  ", and is used for providing, maintaining, and improving the reCAPTCHA service and for general security purposes (it is not used for personalized advertising by Google)."
+                }
+              </span>
+            </div>
+          </div>
+        </FormOther>
+      </SignInContainer>
+    </div>
+  );
+};
+
+export default SignInPage;
