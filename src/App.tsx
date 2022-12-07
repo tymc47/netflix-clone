0bb13092d858356d3395c6d38dec0aaffd29fa2b
Terry Mak <tymc447@gmail.com>
2022-12-07 22:17:34 +0800

    create App mount and add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
 import React from "react";
+import FrontPage from "./LandingPage";
+import styled from "styled-components";
+import GlobalFonts from "./fonts/fonts";
+import { Routes, Route } from "react-router-dom";
+import SignInPage from "./SignInPage";
+
+const AppMount = styled.div`
+  max-width: 100vw;
+  background-color: black;
+  overflow: hidden;
+`;
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <AppMount>
+      <GlobalFonts />
+      <Routes>
+        <Route path="/" element={<FrontPage />} />
+        <Route path="/login" element={<SignInPage />} />
+      </Routes>
+    </AppMount>
+  );
 }
 
 export default App;
