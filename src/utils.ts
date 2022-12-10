1a1dae5d8518ca4be4b22964b1c9d98f93c9c60b
Terry Mak <tymc447@gmail.com>
2022-12-10 11:06:57 +0800

    add rotate array helper function

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,26 @@
+import { Movie } from "./types";
+
 export const validateEmail = (email: string): boolean => {
   const re = /\S+@\S+\.\S+/;
   return re.test(email);
+};
+
+export const rotateMovieArray = (
+  shift: number,
+  direction: "left" | "right",
+  array: Movie[]
+): Movie[] => {
+  const copy = [...array];
+
+  for (let i = 0; i < shift; i++) {
+    if (direction === "left") {
+      const movie = copy.shift();
+      if (movie) copy.push(movie);
+    } else {
+      const movie = copy.pop();
+      if (movie) copy.unshift(movie);
+    }
+  }
+
+  return copy;
 };
