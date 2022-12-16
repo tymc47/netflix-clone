a0cfe81b8150005761ad19afa54354f7bc083b91
Terry Mak <tymc447@gmail.com>
2022-12-16 22:59:50 +0800

    add api response types and media type

--- src/types.ts
+++ src/types.ts
@@ -1,12 +1,23 @@
-export interface Movie {
+export interface ShowBase {
   backdrop_path: string;
   poster_path: string;
-  title: string;
   id: number;
   overview: string;
-  logo: Logo;
-  video: Video;
+  logo?: Logo;
+  video?: Video;
 }
+
+export interface Movie extends ShowBase {
+  type: "movie";
+  title: string;
+}
+
+export interface TvShow extends ShowBase {
+  type: "tv";
+  name: string;
+}
+
+export type Show = Movie | TvShow;
 
 export interface Logo {
   aspect_ratio: number;
@@ -45,4 +56,58 @@ export interface SliderFilter {
   type?: string;
 }
 
-export type Tab = "home" | "tv" | "movie";
+export type Tab = "home" | "tvshows" | "movies" | "mylist";
+
+export const isTab = (tab: string | undefined): tab is Tab => {
+  if (!tab) return false;
+  if (
+    tab === "home" ||
+    tab === "tvshows" ||
+    tab === "movies" ||
+    tab === "mylist"
+  ) {
+    return true;
+  }
+
+  return false;
+};
+
+export const isShowAPI = (input: MovieAPI | TvAPI): input is ShowAPI => {
+  if (!input) return false;
+
+  return true;
+};
+
+export type User = {
+  token: string;
+  username: string;
+  mylist: Show[];
+};
+
+export type MediaType = "movie" | "tv";
+
+export interface APIBase {
+  backdrop_path: string;
+  id: number;
+  overview: string;
+  poster_path: string;
+  genre_ids: number[];
+  video: boolean;
+}
+
+export interface MovieAPI extends APIBase {
+  adult: boolean;
+  title: string;
+  release_date: string;
+}
+
+export interface TvAPI extends APIBase {
+  name: string;
+  first_air_date: string;
+}
+
+export type ShowAPI = MovieAPI | TvAPI;
+
+export interface APIResponse {
+  results: ShowAPI[];
+}
